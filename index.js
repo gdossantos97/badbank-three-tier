5deb62c09a0b74ac7e0b27dbffd6f85f6ba866b3
Gabriel <gabe.cs1997@gmail.com>
2022-06-29 10:39:42 -0400

    added DAL package w/ docker

--- index.js
+++ index.js
@@ -1,42 +1,103 @@
-const express = require('express')
-const app =     express()
-const cors =    require('cors');
+var express = require('express');
+var app     = express();
+var cors    = require('cors');
+var dal     = require('./dal.js');
+const e = require('express');
 
-// Used to serve static files 
-
+// used to serve static files from public directory
 app.use(express.static('public'));
 app.use(cors());
 
-// Create user account
-
+// create user account
 app.get('/account/create/:name/:email/:password', function (req, res) {
-    res.send({
-        name:       req.params.name,
-        email:      req.params.email,
-        password:   req.params.password
-    });
+
+    // check if account exists
+    dal.find(req.params.email).
+        then((users) => {
+
+            // if user exists, return error message
+            if(users.length > 0){
+                console.log('User already in exists');
+                res.send('User already in exists');    
+            }
+            else{
+                // else create user
+                dal.create(req.params.name,req.params.email,req.params.password).
+                    then((user) => {
+                        console.log(user);
+                        res.send(user);            
+                    });            
+            }
+
+        });
 });
 
-// Login
 
+// login user 
 app.get('/account/login/:email/:password', function (req, res) {
-    res.send({
-        email:      req.params.email,
-        password:   req.params.password
+
+    dal.find(req.params.email).
+        then((user) => {
+
+            // if user exists, check password
+            if(user.length > 0){
+                if (user[0].password === req.params.password){
+                    res.send(user[0]);
+                }
+                else{
+                    res.send('Login failed: wrong password');
+                }
+            }
+            else{
+                res.send('Login failed: user not found');
+            }
+    });
+    
+});
+
+// find user account
+app.get('/account/find/:email', function (req, res) {
+
+    dal.find(req.params.email).
+        then((user) => {
+            console.log(user);
+            res.send(user);
+    });
+});
+
+// find one user by email - alternative to find
+app.get('/account/findOne/:email', function (req, res) {
+
+    dal.findOne(req.params.email).
+        then((user) => {
+            console.log(user);
+            res.send(user);
     });
 });
 
 
-// All accounts function 
+// update - deposit/withdraw amount
+app.get('/account/update/:email/:amount', function (req, res) {
 
+    var amount = Number(req.params.amount);
+
+    dal.update(req.params.email, amount).
+        then((response) => {
+            console.log(response);
+            res.send(response);
+    });    
+});
+
+// all accounts
 app.get('/account/all', function (req, res) {
-    res.send({
-        name:       'peter',
-        email:      'peter@mit.edu',
-        password:   'secret'
+
+    dal.all().
+        then((docs) => {
+            console.log(docs);
+            res.send(docs);
     });
 });
 
 var port = 3000;
 app.listen(port);
-console.log('Running on port' + port);
+console.log('Running on port: ' + port);
--- mongo_test.js
+++ mongo_test.js
@@ -1,34 +1,33 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
+ 
+// connect to mongo
+MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
+  console.log("Connected successfully to server");
 
-MongoClient.connect(url, {useUnifiedTopology: true}, function (err, client) {
-    console.log('Connected! ')
-
-    //Database name 
-
+    // database Name
     const dbName = 'myproject';
     const db = client.db(dbName);
 
-    // New user 
+    // new user
+    var name = 'user' + Math.floor(Math.random()*10000);
+    var email = name + '@mit.edu';
 
-    var name = 'user' + Math.floor(Math.random()*1000);
-    var email = name + '@mit.edu'
-
-    //Insert into customer table 
+    // insert into customer table
     var collection = db.collection('customers');
     var doc = {name, email};
     collection.insertOne(doc, {w:1}, function(err, result) {
-        console.log('Document inserted');
+        console.log('Document insert');
     });
 
-   
-    var customer = db 
+    var customers = db
         .collection('customers')
         .find()
         .toArray(function(err, docs) {
-            console.log('Collection:', docs);
+            console.log('Collection:',docs);
 
-            //Clean up
-            client.close();
-        })
+            // clean up
+            client.close();            
+    });    
+
 });
